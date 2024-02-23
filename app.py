0d28ada5dec8ab27510941fbdbdc8d4926841c88
alice <alice.tempesta.work@gmail.com>
2024-02-23 23:52:12 +0200

    new class

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import weather
 
 app = Flask(__name__)
 
+
 @app.route('/weather', methods=['GET'])
 def get_weather():
     city = request.args.get('city')
@@ -11,6 +12,7 @@ def get_weather():
 
     weather_data = weather.get_weather(city)
     return jsonify(weather_data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- weather.py
+++ weather.py
@@ -1,7 +1,35 @@
 import requests
 import json
-#все, что связанно с погодой в класс
+
+class WeatherHandler:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = 'http://api.openweathermap.org/data/2.5/weather'
+
+    def get_weather(self, city):
+        params = {
+            'q': city,
+            'appid': self.api_key,
+            'units': 'metric',
+        }
+
+        response = requests.get(self.base_url, params=params)
+        data = response.json()
+
+        weather_info = {
+            'city': data['name'],
+            'temperature': data['main']['temp'],
+            'description': data['weather'][0]['description'],
+            'icon': data['weather'][0]['icon']
+        }
+
+        return weather_info
+
+
 def handler(event, context):
+    api_key = 'fc62b7b712b9b6d5d6c657b363890e54'  #ЧТО ДЕЛАТЬ С КЛЮЧОМ
+    weather_handler = WeatherHandler(api_key)
+
     city = event['queryStringParameters']['city']
     if not city:
         return {
@@ -9,7 +37,7 @@ def handler(event, context):
             'body': json.dumps({'error': 'City not provided'})
         }
 
-    weather_data = get_weather(city)
+    weather_data = weather_handler.get_weather(city)
     return {
         'statusCode': 200,
         'headers': {
@@ -17,25 +45,3 @@ def handler(event, context):
         },
         'body': json.dumps(weather_data)
     }
-
-def get_weather(city):
-    api_key = ''
-    base_url = 'http://api.openweathermap.org/data/2.5/weather'
-
-    params = {
-        'q': city,
-        'appid': api_key,
-        'units': 'metric',  
-    }
-
-    response = requests.get(base_url, params=params)
-    data = response.json()
-
-    weather_info = {
-        'city': data['name'],
-        'temperature': data['main']['temp'],
-        'description': data['weather'][0]['description'],
-        'icon': data['weather'][0]['icon']
-    }
-
-    return weather_info
